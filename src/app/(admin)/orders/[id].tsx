ec10968ca84ab482d2f38a05f6fc8a5cec7439ab
JJW1643 <josh.woodward1@outlook.com>
2026-05-19 18:23:05 +0100

    Status selector for the orders on the admins side

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -4,6 +4,7 @@ import orders from '@/assets/data/orders';
 import OrderItemListItem from '@/src/components/OrderItemListItem';
 import Colors from '@/src/constants/Colors';
 import OrderListItem from '@/src/components/OrderListItem';
+import { OrderStatusList } from '@/src/types';
 
 
 const OrderDetailsScreen = () => {
@@ -32,6 +33,38 @@ const OrderDetailsScreen = () => {
                 data={order.order_items}
                 renderItem={({ item }) => <OrderItemListItem item={item} />}
                 contentContainerStyle={{ padding: 10, gap: 10 }}
+                ListFooterComponent={() => <>
+                    <Text style={{ fontWeight: 'bold' }}>Status</Text>
+                    <View style={{ flexDirection: 'row', gap: 5 }}>
+                        {OrderStatusList.map((status) => (
+                        <Pressable
+                            key={status}
+                            onPress={() => console.warn('Update status')}
+                            style={{
+                            borderColor: Colors.light.tint,
+                            borderWidth: 1,
+                            padding: 10,
+                            borderRadius: 5,
+                            marginVertical: 10,
+                            backgroundColor:
+                                order.status === status
+                                ? Colors.light.tint
+                                : 'transparent',
+                            }}
+                            >
+                            <Text
+                            style={{
+                                color:
+                                order.status === status ? 'white' : Colors.light.tint,
+                            }}
+                            >
+                            {status}
+                            </Text>
+                        </Pressable>
+                        ))}
+                    </View>
+                    </>
+                    }
             />
         </View>
     );
@@ -45,4 +78,6 @@ const styles = StyleSheet.create({
         padding: 10,
         gap: 10,
     },
+
+
 });
